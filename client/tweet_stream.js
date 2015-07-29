cc2290e5b6acb5b02523cce64e606813f07984e6
Josh Owens <joshua.owens@gmail.com>
2015-07-29 15:04:17 -0400

    Got tweet input working, along with reactive tweet showing. Fixes #4

---
+++ client/tweet_stream.js
@@ -0,0 +1,21 @@
+Template.tweetStream.helpers({
+  tweets: function() {
+    return Tweets.find();
+  }
+});
+
+Template.tweetStream.events({
+  "submit #tweetForm": function(event, template) {
+    event.preventDefault();
+    var text = template.$('.tweet-text').val();
+    Tweets.insert({text: text}, function(error, result) {
+      if (result) {
+        CoffeeAlerts.success('Your tweet has been added.');
+        template.$('.tweet-text').val(null);
+      } else {
+        CoffeeAlerts.warning('There was a problem adding your tweet, try again later.');
+        console.log(error);
+      }
+    });
+  }
+});
