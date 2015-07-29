bf8d6a245d60fa9ebffe3a36ea314a452ba6ed99
Josh Owens <joshua.owens@gmail.com>
2015-07-29 16:18:36 -0400

    Finish up the follow button. Fixes #5

--- client/profile.js
+++ client/profile.js
@@ -1,11 +1,17 @@
 Template.profile.helpers({
   following: function() {
     return _(Meteor.user().profile.followingIds).contains(this.user._id);
+  },
+  notMe: function() {
+    return this.user._id != Meteor.userId();
   }
 });
 
 Template.profile.events({
   'click .follow': function(e, t) {
     Meteor.call('follow', t.data.user._id);
+  },
+  'click .unfollow': function(e, t) {
+    Meteor.call('unfollow', t.data.user._id);
   }
 });
--- collections/users.js
+++ collections/users.js
@@ -5,5 +5,8 @@ Users.helpers();
 Meteor.methods({
   follow: function(followId) {
     Users.update(this.userId, {$push: {"profile.followingIds": followId}});
+  },
+  unfollow: function(followId) {
+    Users.update(this.userId, {$pull: {"profile.followingIds": followId}});
   }
 });
