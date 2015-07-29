44ec1735e9b679bc2849599d9a365cddc15c55dd
Josh Owens <joshua.owens@gmail.com>
2015-07-29 16:51:21 -0400

    Finish the new tweet notification. Fixes #7.

--- client/tweets.js
+++ client/tweets.js
@@ -1,5 +1,19 @@
+Template.tweets.onCreated(function() {
+  Session.set('lastSeenTweets', new Date());
+});
+
 Template.tweets.helpers({
   tweets: function() {
-    return Tweets.find({}, {sort: {tweetedAt: -1}});
+    return Tweets.find({tweetedAt: {$lt: Session.get('lastSeenTweets')}}, {sort: {tweetedAt: -1}});
+  },
+  newTweets: function() {
+    return Tweets.find({tweetedAt: {$gt: Session.get('lastSeenTweets')}}, {sort: {tweetedAt: -1}});
+  }
+});
+
+Template.tweets.events({
+  'click .show-new-tweets': function(e, t) {
+    e.preventDefault();
+    Session.set('lastSeenTweets', new Date());
   }
 });
