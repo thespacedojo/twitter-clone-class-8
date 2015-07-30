d67a0b5df0d0a4ec958be126818787f70af04e71
Josh Owens <joshua.owens@gmail.com>
2015-07-30 13:30:53 -0400

    Get reactive joins working. Fixes #8

--- server/publications.js
+++ server/publications.js
@@ -1,19 +1,55 @@
 Meteor.publish('myTweets', function() {
-  if (this.userId) {
+    var userCursor = Users.find({_id: this.userId});
+    var user = userCursor.fetch()[0];
     var cursors = [];
-    var user = Users.findOne(this.userId);
-    var followingIds = [];
-    followingIds.push(user.profile.followingIds);
-    followingIds.push(this.userId);
-    followingIds = _(followingIds).flatten();
-    users = Users.find({_id: {$in: followingIds}}, {fields: {profile: 1, username: 1}});
-    tweets = Tweets.find({userId: {$in: followingIds}});
-    cursors.push(tweets);
-    cursors.push(users);
+    var ids = [];
+    var self = this;
+    ids.push(user.profile.followingIds);
+    ids.push(self.userId);
+    var followingIds = _.flatten(ids);
+    cursors.push(Tweets.find({userId: {$in: followingIds}}, {sort: {tweetedAt: -1}}));
+    cursors.push(Users.find({
+      _id: {$in: followingIds}
+    }, {
+      fields: {username: 1, "profile.name": 1}
+    }));
+
+    userCursor.observeChanges({
+      changed: function(id, user) {
+        ids = user.profile.followingIds;
+        ids.push(self.userId);
+        debugger
+        flatIds = _.flatten(ids);
+        addedFollowingIds = _.difference(flatIds, followingIds);
+        removedFollowingIds = _.difference(followingIds, flatIds);
+        followingIds = flatIds;
+        // console.log(removedFollowingIds);
+        if (addedFollowingIds) {
+          users = Users.find({_id: {$in: addedFollowingIds}}, {fields: {username: 1, "profile.name": 1}});
+          _.each(users.fetch(), function(user) {
+            console.log(user);
+            self.added('users', user._id, user);
+            tweets = Tweets.find({userId: user._id}, {sort: {tweetedAt: -1}});
+            tweets.forEach(function(tweet) {
+              console.log(tweet);
+              self.added('tweets', tweet._id, tweet);
+            });
+          });
+        }
+        if (removedFollowingIds) {
+          users = Users.find({_id: {$in: removedFollowingIds}}, {fields: {username: 1, "profile.name": 1}});
+          _.each(users.fetch(), function(user) {
+            self.removed('users', user._id);
+            tweets = Tweets.find({userId: user._id});
+            tweets.forEach(function(tweet) {
+              self.removed('tweets', tweet._id);
+            });
+          });
+        }
+      }
+    });
+
     return cursors;
-  } else {
-    this.ready();
-  }
 });
 
 Meteor.publish('profile', function(username) {
